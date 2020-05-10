2f4f902cf550ab59566834935453a201cf3ef4df
Gleb Sterkin <gleb.sterkin@gmail.com>
2020-05-10 15:52:44 +0300

    stuff

--- doper/sim/ball_in_field/ball_in_field.py
+++ doper/sim/ball_in_field/ball_in_field.py
@@ -1,9 +1,9 @@
 """
-all the equations below assumes that the agent is a ball with radius R, and the potential force
+All the equations below assumes that the agent is a ball with radius R, and the potential force
 is being produced only by L2 distance between current agents location and target point.
 The presence of rolling friction is also assumed
 """
-from typing import Tuple, List
+#from typing import Tuple, List
 
 import taichi as ti
 import numpy as np
@@ -12,13 +12,19 @@ import numpy as np
 ti.init(arch=ti.cpu)
 
 screen_res = (800, 400)
-g = 9.8
-
+constants = {
+    'radius': 0.05,
+    'g': 9.8,
+    'f': 0.001,
+    'ro': 1000,
+    }
+constants['volume'] = 4 * np.pi * (constants['radius'] ** 3) / 3
+constants['mass'] = constants['volume'] * constants['ro']
 
 @ti.func
-def compute_potential(curr_coordinate: ti.var,
-                      target_coordinate: ti.var,
-                      ) -> float:
+def compute_potential(curr_coordinate,
+                      target_coordinate,
+                      ):
     """Computes L2 potential value
 
     Args:
@@ -33,9 +39,9 @@ def compute_potential(curr_coordinate: ti.var,
 
 
 @ti.func
-def cumpute_l2_force(curr_coordinate: ti.var,
-                     target_coordinate: ti.var,
-                     ) -> float:
+def cumpute_l2_force(curr_coordinate,
+                     target_coordinate,
+                     ):
     """Computes force produced by L2 potential
 
     Args:
@@ -49,12 +55,12 @@ def cumpute_l2_force(curr_coordinate: ti.var,
 
 
 @ti.func
-def compute_rolling_friction_force(velocity_direction: int,
-                                   mass: float,
-                                   g: float,
-                                   f: float,
-                                   radius: float,
-                                   ) -> float:
+def compute_rolling_friction_force(velocity_direction,
+                                   mass,
+                                   g,
+                                   f,
+                                   radius,
+                                   ):
     """Computes rolling friction force value, flat land assumed
 
     Args:
@@ -73,9 +79,9 @@ def compute_rolling_friction_force(velocity_direction: int,
 
 
 @ti.func
-def compute_acceleration(forces: List,
-                         mass: float,
-                         ) -> float:
+def compute_acceleration(forces,
+                         mass,
+                         ):
     """compute acceleration given list of forces and mass
 
     Args:
@@ -88,16 +94,15 @@ def compute_acceleration(forces: List,
     total_force = 0
     for f in forces:
         total_force += f
-    return total_force / m
+    return total_force / mass
 
 
 @ti.func
-def sim_step(curr_coordinate: ti.var,
-             target_coordinate: ti.var,
-             velocity: ti.var,
-             constants: dict,
-             dt: float,
-             ) -> Tuple[ti.var, ti.var]:
+def sim_step(curr_coordinate,
+             target_coordinate,
+             velocity,
+             dt,
+             ):
     """Makes one step of the simulation
 
     Args:
@@ -129,18 +134,40 @@ def sim_step(curr_coordinate: ti.var,
 
 
 @ti.kernel
-def run_simulation(x0, v0, target_coordinate):
+def run_simulation(x: ti.f32,
+                   v: ti.f32,
+                   target_coordinate: ti.f32):
     t0 = 0
-    x = x0.copy()
-    v = v0.copy()
-    for idx, t1 in np.linspace(0, 20, 4000):
+    for t1 in range(0, 20, 4000):
         dt = t1 - t0
-        update_state()
+        x, v = sim_step(curr_coordinate=x,
+                        target_coordinate=target_coordinate,
+                        velocity=v,
+                        constants=constants,
+                        dt=dt,
+                        )
+        t0 = t1
 
 
-def render(gui):
-    canvas = gui.canvas
-    canvas.clear(bg_color)
-    pos_np = x.to_numpy()
-    gui.circles(pos_np, radius=particle_radius, color=particle_color)
-    gui.show()
+# def render(gui):
+#     canvas = gui.canvas
+#     canvas.clear(bg_color)
+#     pos_np = x.to_numpy()
+#     gui.circles(pos_np, radius=particle_radius, color=particle_color)
+#     gui.show()
+
+@ti.layout
+def place():
+    ti.root.place(x0, v0, target_coordinate)
+
+
+if __name__ == '__main__':
+    x0 = ti.Vector(2, dt=ti.f32)
+    target_coordinate = ti.Vector(2, dt=ti.f32)
+    v0 = ti.Vector(2, dt=ti.f32)
+
+    x0[None] = [2., 0.5]
+    target_coordinate[None] = [0., 0.]
+    v0[None] = [0.1, 0.5]
+
+    run_simulation(x0, v0, target_coordinate)
