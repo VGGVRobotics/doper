1d07f90f585dbe8d2bb95fdfb5d088f965d8c43d
belkakari <sterkin.gleb@gmail.com>
2020-05-15 15:02:54 +0300

    finally working with gui

--- doper/sim/ball/ball_in_field.py
+++ doper/sim/ball/ball_in_field.py
@@ -12,7 +12,7 @@ ti.init(arch=ti.cpu, default_fp=ti.f32)
 gui = ti.GUI("ball", (1024, 1024))
 
 constants = {
-    "radius": 0.05,
+    "radius": 0.1,
     "g": 9.8,
     "f": 0.001,
     "ro": 1000,
@@ -43,29 +43,30 @@ def cumpute_l2_force(t,):
     """Computes force produced by L2 potential
 
     Args:
-        t (int): current timestamp
+        t (int): time id
 
     Returns:
         float: the amount of force produced by L2 potential
     """
-    return 2 * (target_x - x[t, 0])
+    return 2 * (target_x - x[t - 1, 0])
 
 
 @ti.func
-def compute_rolling_friction_force(velocity_direction,):
+def compute_rolling_friction_force(t,):
     """Computes rolling friction force value, flat land assumed
 
     Args:
-        velocity_direction (int): velocity sign, [-1, 0 or 1]
-        mass (float): balls mass, kilogram
-        g (float): gravitational acceleration, metre / second ^ 2)
-        f (float): rolling friction coefficient, metre
-        radius (float): balls radius, metre
+        t (int): time id
 
     Returns:
         float: the amount of the rolling friction force
     """
     normal_force = mass * g
+
+    velocity_direction[None] = v[t - 1, 0]
+    velocity_direction[None][0] /= ti.abs(velocity_direction[None][0])
+    velocity_direction[None][1] /= ti.abs(velocity_direction[None][1])
+
     return -velocity_direction * f * normal_force / radius
 
 
@@ -74,49 +75,50 @@ def sim_step(t: ti.i32,):
     """Makes one step of the simulation
 
     Args:
-        t (float): time id
+        t (int): time id
 
     Returns:
         list: [description]
     """
     l2_force = cumpute_l2_force(t,)
     friction_force = compute_rolling_friction_force(t,)
-    acceleration[t, 0] = (l2_force + friction_force) / mass
+    print(l2_force[0])
+    print(friction_force[0])
+    acceleration[t, 0] = (world_scale_coeff * l2_force + friction_force) / mass
 
     v[t, 0] = v[t - 1, 0] + acceleration[t, 0] * dt
     x[t, 0] = x[t - 1, 0] + v[t, 0] * dt
 
 
-#@ti.kernel
 def run_simulation():
     for t in range(1, sim_steps):
         sim_step(t)
 
         gui.clear(0x3C733F)
 
-        gui.circle(target_x[None], radius=50 // 2, color=0x00000)
+        gui.circle(target_x[None], radius=5, color=0x00000)
 
-        gui.circle(x[t, 0], radius=50, color=0xF20530)
+        gui.circle(x[t, 0], radius=10, color=0xF20530)
 
         gui.show()
-        print(x[t, 0][0])
-        print(x[t, 0][1])
-        print(v[t, 0][0])
-        print(v[t, 0][1])
-        print(target_x[None][1])
+
+        print(x[t, 0][0], x[t, 0][1])
+
 
 @ti.layout
 def place():
     ti.root.dense(ti.l, sim_steps).dense(ti.i, 1).place(x, v, acceleration)
-    ti.root.place(target_x, l2_force, friction_force)
+    ti.root.place(target_x, l2_force, friction_force, velocity_direction)
     ti.root.place(dt, radius, g, f, ro, volume, mass)
 
 
 if __name__ == "__main__":
-    sim_steps = 40
-    max_time = 20
+    sim_steps = 4000
+    max_time = 50
+    world_scale_coeff = 10
 
     target_x = ti.Vector(2, dt=ti.f32)
+    velocity_direction = ti.Vector(2, dt=ti.f32)
     x = ti.Vector(2, dt=ti.f32)
     v = ti.Vector(2, dt=ti.f32)
     acceleration = ti.Vector(2, dt=ti.f32)
@@ -138,9 +140,9 @@ if __name__ == "__main__":
     volume[None] = constants["volume"]
     mass[None] = constants["mass"]
 
-    x[0, 0] = [2.0, 0.5]
-    target_x[None] = [0.0, 0.0]
-    v[0, 0] = [0.1, 0.5]
-    acceleration[0, 0] = [0, 0]
+    x[0, 0] = [0.9, 0.9]
+    target_x[None] = [0.5, 0.5]
+    v[0, 0] = [0.1, -0.1]
+    acceleration[0, 0] = [0.0, 0.0]
     dt[None] = max_time / sim_steps
     run_simulation()
