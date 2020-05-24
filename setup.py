9d2071a67b99951b97a56c90731a678acb8e7993
Vladimir Aliev <twoleggedeye@yandex.ru>
2020-05-24 21:31:46 +0300

    Add primitive setup script

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="doper",
+    version="0.0.1",
+    description="End-to-end locomotion learning with differentiable dynamics",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "taichi==0.6.5",
+        "torch==1.4.0",
+        "numpy==1.18.1",
+        "svgpathtools",
+        "matplotlib",
+    ],
+)
