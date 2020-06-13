6cf3889982327d884527e5e20d6a10a5e805946d
belkakari <sterkin.gleb@gmail.com>
2020-06-13 22:36:35 +0300

    training config

--- scripts/generate_maps.py
+++ scripts/generate_maps.py
@@ -1,8 +1,8 @@
 import os
 
-import numpy as np
+import numpy as onp
 from svgpathtools import Line, Path, wsvg
-
+from multiprocessing import Pool
 
 def self_intersects(line, lines):
     for l in lines:
@@ -15,32 +15,34 @@ def self_intersects(line, lines):
 
 
 min_num_figures = 2
-max_num_figures = 7
+max_num_figures = 20
 min_edge = .1
 max_edge = 5.
 min_field = -10.
 max_field = 10.
 output_folder = '../assets/'
-num_maps = 3
+num_maps = 100
 
-for map_id in range(num_maps):
+def generate_maps(map_id):
+    onp.random.seed(map_id)
     figures = []
     all_lines = []
-    num_figures = np.random.randint(min_num_figures, max_num_figures, 1)[0]
+    num_figures = onp.random.randint(min_num_figures, max_num_figures, 1)[0]
+    print(map_id, num_figures)
     while len(figures) < num_figures:
-        num_lines = np.random.randint(2, 9, 1)[0]
+        num_lines = onp.random.randint(2, 9, 1)[0]
         lines = []
         while len(lines) < num_lines:
             if len(lines) == 0:
-                initial = np.random.uniform(min_field, max_field, 2)
-                direction = np.random.uniform(min_field, max_field, 2)
-                step = direction * np.random.uniform(min_edge, max_edge, 1) / np.linalg.norm(direction)
+                initial = onp.random.uniform(min_field, max_field, 2)
+                direction = onp.random.uniform(min_field, max_field, 2)
+                step = direction * onp.random.uniform(min_edge, max_edge, 1) / onp.linalg.norm(direction)
                 second = initial + step
                 line = Line(initial[0] + initial[1] * 1j, second[0] + second[1] * 1j)
             else:
                 prev_line = lines[-1]
-                direction = np.random.uniform(min_field, max_field, 2)
-                step = direction * np.random.uniform(min_edge, max_edge, 1) / np.linalg.norm(direction)
+                direction = onp.random.uniform(min_field, max_field, 2)
+                step = direction * onp.random.uniform(min_edge, max_edge, 1) / onp.linalg.norm(direction)
                 new_line_end = (prev_line.end.real, prev_line.end.imag) + step
                 line = Line(prev_line.end, new_line_end[0] + new_line_end[1] * 1j)
 
@@ -51,6 +53,7 @@ for map_id in range(num_maps):
                 pass
 
         final_line = Line(lines[-1].end, lines[0].start)
+        # TODO do not discard figure, add more points instead
         if not self_intersects(final_line, all_lines):
             lines.append(final_line)
             figures.append(Path(*lines))
@@ -58,4 +61,8 @@ for map_id in range(num_maps):
             wsvg(paths=figures,
                  filename=os.path.join(output_folder, f"map_{map_id}.svg"),
                  dimensions=(10, 10))
-            print(len(figures))
+
+
+if __name__ == '__main__':
+    with Pool(16) as pool:
+        pool.map(generate_maps, range(num_maps))
